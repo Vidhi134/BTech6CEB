35b6f51e3ea6645df567fa11a3d45a854d17fb7d
Vidhi134 <vidhikathiriya509@gmail.com>
2024-12-24 14:01:17 +0530

    Timtable

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Timetable from './lec2412/timetable'
 //import Component1 from './lec2412/component1'
-import HtmlComponent from './lec2412/htmlcomp'
+//import HtmlComponent from './lec2412/htmlcomp'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HtmlComponent/>
+    <Timetable/>
   </StrictMode>,
 )
