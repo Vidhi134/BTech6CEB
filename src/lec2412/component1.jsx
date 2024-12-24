43d10649a441c92039f039fb8b2ec11fc1a52c58
Vidhi134 <vidhikathiriya509@gmail.com>
2024-12-24 13:32:00 +0530

    new com..

--- src/lec2412/component1.jsx
+++ src/lec2412/component1.jsx
@@ -1,8 +1,31 @@
 import * as React from "react";
+class Component2 extends React.Component {
+    render() {
+        return(
+        <div>
+            <Component3/>
+            <h1>Component2</h1>
+            <h2>World</h2>
+        </div>
+        )
+    }
+}
+
+class Component3 extends React.Component {
+    render() {
+        return(
+        <div>
+            <h1>Component3</h1>
+            <h2>React</h2>
+        </div>
+        )
+    }
+}
 class Component1 extends React.Component {
     render() {
         return(
         <div>
+            <Component2 />
             <h1>Component1</h1>
             <h2>Hello</h2>
         </div>
