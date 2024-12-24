7120451e19d5a82e8aa867bd533080685c87efbd
Vidhi134 <vidhikathiriya509@gmail.com>
2024-12-24 13:12:26 +0530

    component1

---
+++ src/lec2412/component1.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+class Component1 extends React.Component {
+    render() {
+        return(
+        <div>
+            <h1>Component1</h1>
+            <h2></h2>
+        </div>
+        )
+    }
+};
+export default Component1;
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Component1 from './lec2412/component1'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    Hello World from ...........!!Good Morning ..
+    <Component1/>
   </StrictMode>,
 )
