da858dadc2972e14573a4d2b4e4e1fdb7333e526
Vidhi134 <vidhikathiriya509@gmail.com>
2024-12-31 13:43:13 +0530

    first commit

---
+++ src/lec3112/purefunc1.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+export default({var1, var2}) => 
+    <button disabled={var1}>
+        {var2}
+    </button>
+
+
+
+
+//<Purefunc1 var1={true} var2={"hello"}/>
+//<Purefunc1 var1={true} var2={"world"}/>
+//import Purefunc1 from './lec3112/purefunc1'
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import MyCustButton from './lec3112/purefunc2'
+//import Purefunc1 from './lec3112/purefunc1'
 //import Timetable from './lec2412/timetable'
 //import StateABC from './lec3012/statement1'
 //import StateXYZ from './lec3012/statement2'
-import StateMerge from './lec3012/statement3'
+//import StateMerge from './lec3012/statement3'
+// import MyCustComp from './lec3112/propsexmp3'
 //import Component1 from './lec2412/component1'
 //import HtmlComponent from './lec2412/htmlcomp'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <StateMerge/>
+    {/* <MyCustComp/> */}
+    {/* <Purefunc1 var1={true} var2={"hello"}/>
+    <Purefunc1 var1={true} var2={"world"}/> */}
+    <MyCustButton var1={true}/>
+    <br />
+    <MyCustButton var2={"hello"}/>
+    <br />
+    <MyCustButton var1={true} var2={"world"}/>
   </StrictMode>,
 )
